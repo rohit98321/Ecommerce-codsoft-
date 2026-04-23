9ab923cb8191f8673a66fba238d482d45a1634b7
Rohit Gupta <rohitgupta.dev01@gmail.com>
2026-04-23 21:46:38 +0530

    something done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,17 @@
 const express =require("express")
+const userAuthRouter=require("./routes/auth.user")
+const cookieParser=require("cookie-parser")
 
 
 const app=express()
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+app.use("/auth",userAuthRouter)
 
 
 
